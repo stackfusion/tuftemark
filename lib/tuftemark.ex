9cb652ad98417a042049449faf4e9a06696e6930
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-06-17 22:53:49 +0200

    Split content into <section>s around H2 tags
    To make output HTML applicable for Tufte CSS set of styles and expected
    layout, we must split the whole content into chunks around each H2 tag
    and wrap them into `<section>...</section>` blocks.

--- lib/tuftemark.ex
+++ lib/tuftemark.ex
@@ -1,17 +1,43 @@
 defmodule Tuftemark do
-  @moduledoc false
+  @moduledoc """
+  Tuftemark converts Markdown content into format that is suitable to be used with Tufte CSS set of styles.
 
-  alias Earmark.{Options, Parser, Transform}
+  Tufte CSS layout requires a few things to be applied to the HTML, for example:
 
+  - the whole post must be wrapped in `<article>` tag
+  - content must be split into `<section>`s around every H2 tag
+
+  The `as_html!/2` method is trying to apply all the given transformation to the originally parsed AST,
+  so we get applicable HTML output in the end.
+  """
+
+  alias Earmark.{Options, Parser, Restructure, Transform}
+
+  @doc """
+  Expects a [GitHub Flavored Markdown](https://github.github.com/gfm/) as first argument and list of options
+  applicable to [`Earmark.as_html!/2](https://hexdocs.pm/earmark/Earmark.html#as_html!/2) to pass into other
+  Earmark-related modules.
+  """
   def as_html!(markdown, opts \\ []) do
     {:ok, ast, _warnings} = Parser.as_ast(markdown)
 
     options = Options.make_options!(opts)
 
     ast
+    |> Restructure.walk_and_modify_ast([], &make_section/2)
+    |> last_section()
     |> wrap_in("article")
     |> Transform.transform(options)
   end
 
   defp wrap_in(ast, tagname), do: [{tagname, [], ast, %{}}]
+
+  defp make_section({"h2", _, _, _} = item, acc),
+    do: {wrap_in(Enum.reverse(acc), "section"), [item]}
+
+  defp make_section(item, acc),
+    do: {[], [item | acc]}
+
+  defp last_section({ast, acc}),
+    do: ast ++ wrap_in(Enum.reverse(acc), "section")
 end
--- test/tuftemark_test.exs
+++ test/tuftemark_test.exs
@@ -20,10 +20,53 @@ defmodule TuftemarkTest do
 
     expected = """
     <article>
-      <h1>Lorem</h1>
-      <p>Ipsum!</p>
-      <h2>Sit dolor</h2>
-      <p>Amet.</p>
+      <section>
+        <h1>Lorem</h1>
+        <p>Ipsum!</p>
+      </section>
+      <section>
+        <h2>Sit dolor</h2>
+        <p>Amet.</p>
+      </section>
+    </article>
+    """
+
+    assert compact_html(expected) == Tuftemark.as_html!(markdown, compact_output: true)
+  end
+
+  # @tag :skip
+  test "split content into sections around every h2 tag" do
+    markdown = """
+    # Class vehicula
+
+    Torquent convallis inceptos.
+
+    ## Erat ipsum eros
+
+    ### Nullam velit
+
+    Dapibus mollis pharetra morbi.
+
+    ## Habitasseduis
+
+    Duis orci.
+    """
+
+    expected = """
+    <article>
+      <section>
+        <h1>Class vehicula</h1>
+        <p>Torquent convallis inceptos.</p>
+      </section>
+      <section>
+        <h2>Erat ipsum eros</h2>
+        <h3>Nullam velit</h3>
+        <p>Dapibus mollis pharetra morbi.</p>
+      </section>
+      <section>
+        <h2>Habitasseduis</h2>
+        <p>Duis orci.</p>
+      </section>
     </article>
     """
 
