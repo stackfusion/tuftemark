6f54094e3c8259ac3c281234f9d02843aea4a3d5
Sergey Kuznetsov <kuznecov.sg@gmail.com>
2024-07-03 23:29:21 +0200

    Refactor Tuftemark.Sidenotes processor
    As an intermediary step for the coming changes to Blockquotes processor,
    we found (and eventually fixed) an annoying issue in the Sidenotes one:
    
    In some cases, we get nested lists of children elements (because we
    processed text nodes previously and replaced them with lists of marginal
    notes elements).
    
    So, we fixed that and made Sidenotes module code cleaner by giving
    better names to the arguments, variables, functions, splitting code into
    more granular logical pieces, etc.
    
    Enjoy! :)

--- lib/tuftemark/sidenotes.ex
+++ lib/tuftemark/sidenotes.ex
@@ -2,40 +2,54 @@ defmodule Tuftemark.Sidenotes do
   alias Earmark.{Restructure, Transform}
   alias Tuftemark.Utils
 
-  @foonote_prefix ~r/\[\^(.+)\]/
+  @parse_ex ~r/^\[\^(.*?)\]: (.*)/
+  @split_ex ~r/\[\^(.*?)\]/
 
   def process(ast) do
-    with {cleaner_ast, footnotes} <- find_and_clean(ast),
-         converted_footnotes <- convert(footnotes) do
-      modify(cleaner_ast, converted_footnotes)
+    with {cleaner_ast, footnotes} <- find_footnotes(ast),
+         sidenotes <- to_sidenotes(footnotes) do
+      expand_footnotes(cleaner_ast, sidenotes)
     end
   end
 
-  # Collect all paragraph-footnotes and remove them from the original AST
-  defp find_and_clean(ast) do
+  # Collects all footnote-paragraphs and removes them from the original AST
+  defp find_footnotes(ast) do
     Restructure.walk_and_modify_ast(ast, %{}, fn
-      {"p", _, children, _} = node, acc ->
-        # some paragraphs start not with a text, but with an image or other tag
-        # this is not what we're looking for here, we need only: `[^note]: ...`
+      {_, _, children, _} = node, footnotes_acc when children != [] ->
+        # Some paragraphs start not with a text, but with an image or other tag
+        # this is not what we're looking for here, we need only: `[^note]: ...`.
+        #
+        # So, (1) we take the first element and (2) check if it's a string, then
+        # (3) we try to parse this piece of text; (4) if it can be parsed into
+        # two parts, then it's likely a beginning of a footnote - we grab its
+        # key and content (cleaned from the key).
+        #
+        # Then we re-combine all the details.
+        #
+        # If anything goes wrong: skip it and continue...
         with maybe_text <- hd(children),
              true <- is_binary(maybe_text),
-             [note_id, note_clean] <-
-               Regex.run(~r/^\[\^(.+)\]: (.*)/, maybe_text, capture: :all_but_first) do
-          new_children = List.update_at(children, 0, fn _ -> note_clean end)
-          new_node = put_elem(node, 2, new_children)
+             [key, content] <- parse_footnote(hd(children)) do
+          footnote_children = [content | tl(children)]
+          footnote = put_elem(node, 2, footnote_children)
 
-          {[], Map.put(acc, note_id, new_node)}
+          {[], Map.put(footnotes_acc, key, footnote)}
         else
-          _ -> {node, acc}
+          _ ->
+            {node, footnotes_acc}
         end
 
-      node, acc ->
-        {node, acc}
+      node, footnotes_acc ->
+        {node, footnotes_acc}
     end)
   end
 
-  # Convert footnote's layout (likely, from a `p` tag into set of three tags)
-  defp convert(footnotes) do
+  # - Does it look like "[^note]: Lorem ipsum..."? Let's figure this out!
+  defp parse_footnote(text),
+    do: Regex.run(@parse_ex, text, capture: :all_but_first)
+
+  # Converts footnote's layout into set of three tags
+  defp to_sidenotes(footnotes) do
     footnotes
     |> Enum.map(fn {key, {_, attrs, children, meta}} ->
       is_numbered = String.starts_with?(key, "-")
@@ -43,34 +57,48 @@ defmodule Tuftemark.Sidenotes do
 
       {key, sidenote}
     end)
-    |> Enum.into(%{})
+    |> Map.new()
   end
 
-  # Eventually, we can postprocess previously cleaned AST, find footnotes inside
-  # its nodes and replace them with ones from the map of converted footnotes.
-  defp modify(cleaner_ast, footnotes_found) do
-    Transform.map_ast(cleaner_ast, fn
-      node when is_binary(node) ->
-        if String.match?(node, @foonote_prefix) do
-          node
-          |> String.split(@foonote_prefix, include_captures: true)
-          |> Enum.map(fn piece ->
-            case Regex.run(@foonote_prefix, piece, capture: :all_but_first) do
-              [footnote_key] ->
-                # if we cannot find a footnote in the given map (due to a
-                # possible TYPO), we won't replace it and leave as is
-                Map.get(footnotes_found, footnote_key, piece)
+  # Eventually, we can process the previously cleaned AST, search for footnotes'
+  # anchors inside the text nodes and replace them with elements from the stash
+  # of the prepared sidenotes.
+  #
+  # We look into non-string nodes (the `true` last argument of the `map_ast/3`)
+  # to avoid the issue when may get nested lists (`[[...]]`) for children. By
+  # the way, that is why we `flatten` the resulting list of node's children in
+  # the `maybe_expand/2` function.
 
-              _otherwise ->
-                piece
-            end
-          end)
-        else
-          node
-        end
+  defp expand_footnotes(cleaner_ast, sidenotes),
+    do: Transform.map_ast(cleaner_ast, &maybe_expand(&1, sidenotes), true)
 
-      node ->
-        node
-    end)
+  defp maybe_expand({tag, attrs, children, meta}, sidenotes) do
+    new_children =
+      children
+      |> Enum.map(fn
+        node when is_binary(node) ->
+          node
+          |> String.split(@split_ex, include_captures: true)
+          |> Enum.map(&maybe_replace(&1, sidenotes))
+
+        node ->
+          node
+      end)
+      |> List.flatten()
+
+    {:replace, {tag, attrs, new_children, meta}}
+  end
+
+  # Here we attempt to clean the sidenote key from what we got after splitting:
+  # if we can parse "lorem-ipsum" from the given "[^lorem-ipsum]" then we try to
+  # replace this piece with a sidenote elements, or skip trying...
+  defp maybe_replace(piece, sidenotes) do
+    case Regex.run(@split_ex, piece, capture: :all_but_first) do
+      [sidenote_key] ->
+        Map.get(sidenotes, sidenote_key, sidenote_key)
+
+      _otherwise ->
+        piece
+    end
   end
 end
